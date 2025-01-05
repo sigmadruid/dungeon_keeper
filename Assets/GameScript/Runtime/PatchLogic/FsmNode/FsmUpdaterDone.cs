7904cbceecd58f2eef3b1c3d17dfd4a2343c34bc
hai.xie <hai.xie@funplus.com>
2025-01-05 16:14:29 +0800

    first commit

---
+++ Assets/GameScript/Runtime/PatchLogic/FsmNode/FsmUpdaterDone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniFramework.Machine;
+
+/// <summary>
+/// 流程更新完毕
+/// </summary>
+internal class FsmUpdaterDone : IStateNode
+{
+    private PatchOperation _owner;
+
+    void IStateNode.OnCreate(StateMachine machine)
+    {
+        _owner = machine.Owner as PatchOperation;
+    }
+    void IStateNode.OnEnter()
+    {
+        _owner.SetFinish();
+    }
+    void IStateNode.OnUpdate()
+    {
+    }
+    void IStateNode.OnExit()
+    {
+    }
+}
